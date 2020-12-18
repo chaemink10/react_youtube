690e25f0eaebd676f3136e60c253d1640b622594
kyungsun choi <kschoi0308@gmail.com>
2020-12-18 17:18:49 +0900

    fetch 수정

--- src/app.jsx
+++ src/app.jsx
@@ -1,36 +1,32 @@
-import React, { useState } from 'react';
-import './app.css';
+import React, { useState, useEffect } from 'react';
+import './App.css';
 import SearchBar from './components/SearchBar';
 import SearchList from './components/SearchList';
 
+const requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+};
+
 const App = () => {
-  let obj;
-  const requestOptions = {
-    method: 'GET',
-    redirect: 'follow',
-  };
+  const [listData, setListData] = useState();
 
-  fetch(
-    'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyC94Fr5Lg1EjR_JhhuzCCNEqU1UwKQSy_k',
-    requestOptions
-  )
-    .then((response) => response.json())
-    .then((result) => {
-      //obj = result.items;
-      test(result.items);
-    })
-    .catch((error) => console.log('error', error));
-
-  function test(result) {
-    obj = result;
-  }
-  console.log(obj);
-  //const [items, setItems] = useState(obj);
+  useEffect(()=> {
+    fetch(
+      'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyC94Fr5Lg1EjR_JhhuzCCNEqU1UwKQSy_k',
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        setListData(result.items);
+      })
+      .catch((error) => console.log('error', error));
+  }, [])
 
   return (
     <>
       <SearchBar />
-      <SearchList result={items} />
+      <SearchList result={listData} />
       {/* 리스트 - 카드형식*/}
       {/* 리스트상세 - 동영상, 상세, 오른쪽카드목록*/}
     </>
